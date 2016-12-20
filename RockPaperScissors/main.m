8d01deca69355c76679944eda875230bd4c71609
daxapps <daxapps777@gmail.com>
2016-12-20 11:35:25 -0600

    Initial Commit

---
+++ RockPaperScissors/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RockPaperScissors
+//
+//  Created by Jason Crawford on 12/20/16.
+//  Copyright © 2016 Jason Crawford. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
